4dcbb22b9288fdbca97677ca854eea23595b0604
m_ronald <michael.ronald@allycode.com.br>
2022-03-20 14:03:24 -0300

    atualizando retorno de url da imagem

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,14 @@ class ProductController extends Controller
      */
     public function index(): JsonResponse
     {
-        return response()->json(Product::with('reviews')->get());
+        $products = Product::with('reviews')->get()->each(
+            function (Product $product) {
+                $product->photo = asset($product->photo);
+                $product->photo_2 = asset($product->photo_2);
+            }
+        );
+
+        return response()->json($products);
     }
 
     /**
